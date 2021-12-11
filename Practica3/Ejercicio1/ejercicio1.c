a71449974af1c0a4b53377e232c379f42d86ead6
ldiazriod <ldiazriod@alumnos.nebrija.es>
2021-12-11 13:50:41 +0100

    Practica 3 added

--- Practica3/Ejercicio1/ejercicio1.c
+++ Practica3/Ejercicio1/ejercicio1.c
@@ -9,11 +9,11 @@
 #define length 512
 
 int main(int argc, char **argv){
-    pid_t pid;
-    int p[2];
-    char buffer[length];
+    pid_t pid; //Process iden
+    int p[2]; //Declaramos el futuro pipe
+    char buffer[length]; //Un buffer de lectura/escritura para el pipe.
 
-    pipe(p);
+    pipe(p); //Decimos que el p va a ser un pipe.
 
     pid = fork(); // fork() Duplica el código -> 2 procesos. 1 padre, 1 hijo. 2^n -> n = número de forks.
 
@@ -21,20 +21,26 @@ int main(int argc, char **argv){
         printf("No se pudo crear al hijo");
         exit(-1);
     }else if(pid == 0){ //Si se crea y es el hijo
-        close(p[0]);
-        printf("El hijo escribe en el PIPE \n");
+        close(p[0]); //Cerramos la lectura en el pipe.
+        printf("El hijo escribe en el PIPE \n"); 
         sleep(3);
-        sprintf(buffer, "Hola mundo!");
-        write(p[1], buffer, length);
-        close(p[1]);
+        sprintf(buffer, "Hola mundo!"); //Guardamos en el buffer un mensaje.
+        write(p[1], buffer, length); //write() -> p[1] (escritura), buffer con el mensaje, con tamaño length.
+        close(p[1]); //Cerrar la escritura.
     }else{ //Si es el padre.
-        close(p[1]);
-        wait(NULL);
+        close(p[1]); //Cerramos la escritura.
+        wait(NULL); //Esperamos a que termine el proceso hijo.
         printf("Padre leyendo en el PIPE \n");
         sleep(3);
-        read(p[0], buffer, length);
-        printf("Mensaje: %s \n", buffer);
-        close(p[0]);
+        read(p[0], buffer, length); //Leemos de p[0] y guardamos el mensaje dentro del buffer con tamaño length.
+        printf("Mensaje: %s \n", buffer); //Sacamos buffer
+        close(p[0]); //Cerramos lectura.
     }
-    exit(0);
+    exit(0); //Terminamos.
 }
+
+/*
+    Pipes -> Las pipes en Unix tienen solo lectura y escritura, y se activan mediante integers.
+    pipe[0] -> Lectura.
+    pipe[1] -> Escritura.
+*/
--- Practica3/Ejercicio2/ejercicio2.c
+++ Practica3/Ejercicio2/ejercicio2.c
@@ -8,57 +8,171 @@
 
 #define length 12
 
-int buffer[length];
-pthread_t thread1, thread2 ,thmain;
-pthread_attr_t attr;
-sem_t semC, semP;
+int buffer[length]; //Declaramos el buffer
+pthread_t thread1, thread2 ,thmain; //Declaramos los futuros threads
+pthread_attr_t attr; //Declaramos los atributos que más tarde incializaran los threads.
+sem_t semC, semP; //Declaramos los semaforos.
 
 void *producer(void *arg){
-    for(int i=0; i < length; i++){
-        sem_wait(&semP);
+    for(int i=0; i < length; i++){ //for 12
+        sem_wait(&semP); //semp--
         if(buffer[i] == 0){ // Si no hay elemento en buffer[i].
-            buffer[i] = i+1; //inicializa con i+1. -> rango 1-10. 
-            int aux = buffer[i];
-            printf("Dato producido: %d \n", aux);
-            for(int i=0; i < length; i++){
+            buffer[i] = i+1; //inicializa con i+1. -> rango 1-12. 
+            int aux = buffer[i]; //Esto no sirve para nada. Es copia y pega de la función consumer.
+            printf("Dato producido: %d \n", aux); //Decimos que dato hemos producido.
+            for(int i=0; i < length; i++){ //Sacamos el buffer modificado.
                 printf("%d ", buffer[i]);
             }
             printf("\n");
         }
-        sem_post(&semC);
-        sleep(random() % 3);
+        sem_post(&semC); //semC++
     }
-    pthread_exit(NULL);
+    //Al terminar el for. semP = 0; semC = 12.
+    pthread_exit(NULL); //Cerramos el thread.
 }
 
 void *consumer(void *arg){
-    for(int i=12; i > 0; i--){
-        sem_wait(&semC);
-        if(buffer[i] != 0){
-            int aux = buffer[i];
-            buffer[i] = 0;
-            printf("Dato consumido %d \n", aux);
-            for(int i=0; i < length; i++){
+    for(int i=length; i > 0; i--){ //Recorremos el buffer pero al reves.
+        sem_wait(&semC); //semC--
+        if(buffer[i] != 0){ //Solo podemos consumir si el buffer en esa posición está lleno.
+            int aux = buffer[i]; //Guardamos el valor antes de consumirlo, para más tarde sacarlo por pantalla.
+            buffer[i] = 0; //Consumimos el valor. Antes buffer[i] = 3 -> Ahora buffer[i] = 0;
+            printf("Dato consumido %d \n", aux); //Sacamos por pantalla el elemento que hemos consumido usando la variable aux.
+            for(int i=0; i < length; i++){ //Imprimimos por pantalla el nuevo buffer con el elemento consumido.
                 printf("%d ", buffer[i]);
             }
             printf("\n");
         }
-        sem_post(&semP);
-        sleep(random() % 3);
+        sem_post(&semP); //SemP++;
     }
     pthread_exit(NULL);
 }
 
 int main(int argc, char **argv){
-    thmain = pthread_self();
-    pthread_attr_init(&attr);
-    sem_init(&semP, 0, 12);
-    sem_init(&semC, 0, 0);
-    pthread_create(&thread1, &attr, producer, NULL);
-    pthread_create(&thread2, &attr, consumer, NULL);
-    pthread_join(thread1, NULL);
+    thmain = pthread_self(); //Especificamos el thread princial.
+    pthread_attr_init(&attr); //Inicializamos los atributos de los threads.
+    sem_init(&semP, 0, 12); //semP -> Local y empieza en el valor 12. 
+    sem_init(&semC, 0, 0); //SemC -> Local pero empieza en 0.
+    pthread_create(&thread1, &attr, *producer, NULL); //Creamos los threads con los atributos y las funciones.
+    pthread_create(&thread2, &attr, *consumer, NULL);
+    pthread_join(thread1, NULL); //Esperamos a que las funciones de los threads se terminen de ejecutar y los juntamos el thread principal.
     pthread_join(thread2, NULL);
-    sem_destroy(&semP);
+    sem_destroy(&semP); //Ponemos en blanco los semaforos. Los quitados de los espacios de memoria reservados.
     sem_destroy(&semC);
+    pthread_exit(NULL); //Cerramos el thread principal.
+}
+
+
+
+
+
+
+
+
+/*
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <string.h>
+#include <pthread.h>
+#include <semaphore.h>
+
+#define SIZE 12
+
+int buffer[SIZE];
+pthread_t h1, h2, thmain;
+pthread_attr_t attr;
+sem_t sem;
+int cont = 0;
+
+//TODO: Funciones esta vacio o lleno el buffer
+int is_empty(int b[]){
+    for( int i=0; i<SIZE; i++){
+        if(b[i] != 0){
+            return -1; // Return false
+        }
+    }
+    return 0; // Return true
+}
+
+int is_full(int b[]){
+    for( int i=0; i<SIZE; i++){
+        if(b[i] == 0){
+            return -1; // Return false
+        }
+    }
+    return 0; // Return true
+}
+
+//TODO: Funciones producir info en buffer y consumir info en buffer
+int produce_info(int b[]){
+    for(int i=0; i<SIZE; i++){
+        if(is_empty){
+            b[i] = i;
+        }
+    }
+}
+
+int consume_info(int b[]){
+    for(int i=0; i<SIZE; i++){
+        if(is_full){
+            b[i] = 0;
+        }
+    }
+}
+
+
+//TODO: Funciones productor-consumidor
+void *productor(void * arg){
+    for(int i=0; i<SIZE; i++){
+        sem_wait(&sem);
+        if(is_empty){
+            //! Produce informacion
+            produce_info(buffer);
+            printf("Info added to the buffer.\n");
+        }
+        sem_post(&sem);
+    }
     pthread_exit(NULL);
 }
+
+void *consumidor(void * arg){
+    for(int i=0; i<SIZE; i++){
+        sem_wait(&sem);
+        if(is_full){
+            //! Consume informacion
+            consume_info(buffer);
+            printf("Info was consumed.\n");
+        }
+        sem_post(&sem);
+    }
+    pthread_exit(NULL);
+}
+
+
+
+
+int main(void){
+    thmain = pthread_self();
+    pthread_attr_init(&attr);
+    sem_init(&sem, 0, 1);
+    pthread_create(&h1, NULL, *productor, NULL);
+    pthread_create(&h2, NULL, *consumidor, NULL);
+    pthread_join(h1, NULL);
+    pthread_join(h2, NULL);
+    sem_destroy(&sem);
+    pthread_exit(NULL);
+
+    return 0;
+}
+*/
+
+/*
+    Semaforos: Sincronizan.
+    Unix tenemos varias funciones, pero primariamente 2.
+    sem_wait() y sem_post().
+    sem_wait() -> reduce en 1 el semaforo.
+    sem_post() -> Aumenta en 1 el semoforo.
+    sem_init() -> inicializa el semaforo, con el tipo de localidad y el valor inicical.
+
+*/
